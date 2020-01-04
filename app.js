a860deae259e57bb16d6617673c8917abae42aac
Massiel50 <massielescano@yahoo.com>
2020-01-03 20:36:46 -0500

    if statements for each choice

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 var inquirer = require("inquirer");
 var jest = require("jest");
 
-function Employee{
-    this.name = name,
-    this.id = id,
-    this.title = title,
-    this.getName(),
-    this.getId(),
-    this.getEmail(),
-    this.getRole()
-}
+// ask user for role, name, id, and email
+inquirer.prompt([{
+    type: "list",
+    name: "role",
+    message: "What kind of employee would you like to add?",
+    choices: ["Manager", "Engineer", "Intern"]
+}])
+.then(function(choice){
+
+    function Employee{
+        this.name = name,
+        this.id = id,
+        this.email = email;
+        choice.role = title,
+        this.getName(),
+        this.getId(),
+        this.getEmail(),
+        this.getRole()
+    }
+    // if user chose manager, ask for office number, override role to manager with getRole()
+    if(choice == Manager){
+
+    }
+    // if user chose engineer, ask for github user name, getGitHub(), getRole()
+    if(choice == Engineer){
+
+    }
+    // if user chose intern, ask what school, getSchool(), getRole()
+    if(choice == Intern){
+
+    }
+})
+
+
+
