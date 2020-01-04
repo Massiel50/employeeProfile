cc51fd7a079c9d8732bf0c1c32a9580ec10c0158
Massiel50 <massielescano@yahoo.com>
2020-01-03 20:02:12 -0500

    made contr for employee

---
+++ README2.md
@@ -0,0 +1,2 @@
+# employeeProfile
+Generates a profile of employees based the information inputed
---
+++ app.js
@@ -0,0 +1,12 @@
+var inquirer = require("inquirer");
+var jest = require("jest");
+
+function Employee{
+    this.name = name,
+    this.id = id,
+    this.title = title,
+    this.getName(),
+    this.getId(),
+    this.getEmail(),
+    this.getRole()
+}
