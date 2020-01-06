cdbeb825d40724e1fc0d73d2a02f452e341b1aee
Massiel50 <massielescano@yahoo.com>
2020-01-06 09:55:32 -0500

    moved constr to outside of prompt, added questions

--- app.js
+++ app.js
@@ -1,25 +1,45 @@
 var inquirer = require("inquirer");
 var jest = require("jest");
 
+
+function Employee(){
+    this.name = name,
+    this.id = id,
+    this.email = email;
+    // this.getName(){
+    //     return this.name;
+    // }
+    // this.getId(),
+    // this.getEmail(),
+    // this.getRole()
+}
+
+
 // ask user for role, name, id, and email
 inquirer.prompt([{
     type: "list",
     name: "role",
     message: "What kind of employee would you like to add?",
     choices: ["Manager", "Engineer", "Intern"]
+},
+{
+    type: "input",
+    message: "What is the employees name?",
+    name: "name"
+},
+{
+    type: "input",
+    message: "What is the employee ID?",
+    name: "id"
+},
+{
+    type: "input",
+    message: "What is the employee email address?",
+    name: "email"
 }])
-.then(function(role){
+.then(function(role, name, id, email){
 
-    function Employee(){
-        this.name = name,
-        this.id = id,
-        this.email = email;
-        role = title,
-        this.getName(),
-        this.getId(),
-        this.getEmail(),
-        this.getRole()
-    }
+   
     console.log(role)
     // if user chose manager, ask for office number, override role to manager with getRole()
     if(role == Manager){
@@ -34,12 +54,32 @@ inquirer.prompt([{
     }
     // if user chose engineer, ask for github user name, getGitHub(), getRole()
     if(role == Engineer){
+       
+            inquirer.prompt([{
+                type: "input",
+                name: "gitHub",
+                message: "What is the git hub user name?",
+            }])
+            .then(function(git){
+                Employee.prototype.gitHub = function(){
+                    this.gitHub = git;
+                }
+            })
+        }
 
-    }
+    
     // if user chose intern, ask what school, getSchool(), getRole()
     if(role == Intern){
-
+        Employee.prototype.school = function(){
+            inquirer.prompt([{
+                type: "input",
+                name: "school",
+                message: "What school is intern attending?",
+            }])
+        }
     }
+
+
 })
 
 
