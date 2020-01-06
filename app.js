bc82f767b3a41ff71b08a6fc428c4cd266fd8fab
Massiel50 <massielescano@yahoo.com>
2020-01-05 22:14:39 -0500

    added proto to manager if for office number

--- app.js
+++ app.js
@@ -8,28 +8,36 @@ inquirer.prompt([{
     message: "What kind of employee would you like to add?",
     choices: ["Manager", "Engineer", "Intern"]
 }])
-.then(function(choice){
+.then(function(role){
 
-    function Employee{
+    function Employee(){
         this.name = name,
         this.id = id,
         this.email = email;
-        choice.role = title,
+        role = title,
         this.getName(),
         this.getId(),
         this.getEmail(),
         this.getRole()
     }
+    console.log(role)
     // if user chose manager, ask for office number, override role to manager with getRole()
-    if(choice == Manager){
+    if(role == Manager){
+        Employee.prototype.offNum = function(){
+            inquirer.prompt([{
+                type: "input",
+                name: "tel",
+                message: "What is the office number?",
+            }])
+        }
 
     }
     // if user chose engineer, ask for github user name, getGitHub(), getRole()
-    if(choice == Engineer){
+    if(role == Engineer){
 
     }
     // if user chose intern, ask what school, getSchool(), getRole()
-    if(choice == Intern){
+    if(role == Intern){
 
     }
 })
