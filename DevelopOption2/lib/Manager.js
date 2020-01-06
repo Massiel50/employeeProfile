67682748ec2ef5fefcd1bff05680c931c1191f07
Massiel50 <massielescano@yahoo.com>
2020-01-06 15:30:49 -0500

    new files pulled from class repo

---
+++ DevelopOption2/lib/Manager.js
@@ -0,0 +1,13 @@
+// require Employee class
+const Employee = require("./Employee");
+
+// create a class called manager that extends Employee
+
+class Manager extends Employee{
+    constructor(name, id, email, officeNum){
+        super()
+        this.
+    }
+}
+
+// export manager
